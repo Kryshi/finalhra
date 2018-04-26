be1265dbf2d1456fbe91c3405c73a1b449e2d415
KrySt <stejskalkryshi@gmail.com>
2018-04-26 14:07:40 +0200

    Initial commit

---
+++ finalhra.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:26:16 2018
+
+@author: KrySt
+"""
+
+import pyglet
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
